8b6cffd2ea776d1aef8f986609a85a1e9a10271d
Dannyfsp <femoscopilala@gmail.com>
2022-07-05 19:03:54 -0700

    alphabet fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,7 +2,6 @@
 /**
  * print_alphabet - Entry point
  * prints alphabet in lowercase
- * Return: On success 0
  */
 void print_alphabet(void)
 {
@@ -12,5 +11,4 @@ for (i = 'a'; i <= 'z'; i++)
 _putchar(i);
 }
 _putchar('\n');
-return (0);
 }
