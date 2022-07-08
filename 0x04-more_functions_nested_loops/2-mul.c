a9772f9ab2f12e0be8797e40a2453c2403eb096f
Dannyfsp <femoscopilala@gmail.com>
2022-07-07 19:45:36 -0700

    digit and mul commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _isdigit - Entry point
+ * a function to check for a digit(0 through 9)
+ * @a: an integer parameter 1
+ * @b: an integer parameter 2
+ * Return: On success multiplication
+ */
+int mul(int a, int b)
+{
+return (a * b);
+}
