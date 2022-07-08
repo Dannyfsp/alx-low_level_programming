ce7f6371fbf7be0c90f28c18c4af6a2960d13a17
Dannyfsp <femoscopilala@gmail.com>
2022-07-07 19:32:13 -0700

    first commit on more_functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isupper - Entry point
+ * a function to check for uppercase alpha
+ * @c: an integer parameter
+ * Return: On success uppercase
+ */
+int _isupper(int c)
+{
+return (c >= 'A' && c <= 'Z');
+}
---
+++ 0x04-more_functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+A readme for more functions and nested loops tasks by ALX.
