37dfab7a5766c9d2f18a84569992ee4d1005c4ae
Dannyfsp <femoscopilala@gmail.com>
2022-07-12 19:27:53 -0700

    print string

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ * function that prints a string
+ * @str: parameter of a pointer
+ */
+void _puts(char *str)
+{
+	for (; *str != '\0'; str++)
+	{
+		_putchar(*str);
+	}
+	_putchar('\n')
+}
