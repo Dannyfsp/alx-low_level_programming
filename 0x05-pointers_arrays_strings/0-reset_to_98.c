e16407953ca453919ebd6782b8a959e63de8c4ef
Dannyfsp <femoscopilala@gmail.com>
2022-07-12 17:59:43 -0700

    first commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * function that takes a pointer to an int as a parameter and updates the value
+ * @n: pointer to n to be updated
+ */
+void reset_to_98(int *n);
+{
+*n = 98;
+}
