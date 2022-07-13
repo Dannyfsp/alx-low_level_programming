678fbe488bf9658cb23870a3af7f709efb208c94
Dannyfsp <femoscopilala@gmail.com>
2022-07-12 21:31:49 -0700

    advanced on conversion of string to integer

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _atoi - Entry point
+ * function that converts a string to an integer
+ * @s: the pointer parameter to convert
+ * Return: on Success - an integer
+ */
+int _atoi(char *s)
+{
+	int a;
+	unsigned int n = 0;
+	int min = 1;
+	int sec = 0;
+
+	while (s[a])
+	{
+		if (s[a] == 45)
+		{
+			min *= 1;
+		}
+		while (s[a] >= 48 && s[a] <= 57)
+		{
+			sec = 1;
+			n = (n * 10) + (s[a] - '0');
+			a++;
+		}
+		if (sec == 1)
+		{
+			break;
+		}
+	}
+	n *= min;
+	return (n);
+}
