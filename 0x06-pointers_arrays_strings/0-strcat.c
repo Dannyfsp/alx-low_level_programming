e439a3c36baa47e381f349eef276432858267cbf
Dannyfsp <femoscopilala@gmail.com>
2022-07-14 04:29:58 -0700

    initial commit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - Entry point
+ * function that concatenates two strings
+ * @dest: pointer parameter 1
+ * @src: pointer parameter 2
+ * Return: on Success - dest string
+ */
+char *_strcat(char *dest, char *src)
+{
+	dest[];
+	src[];
+	strcat(dest, scr);
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+A readme for more tasks on pointers, arrays and strings centered on functions approach given by ALX
