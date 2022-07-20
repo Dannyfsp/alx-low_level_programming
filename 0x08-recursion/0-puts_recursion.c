197e5f7d24d1f5e57aaf4b00042a375772301beb
Dannyfsp <femoscopilala@gmail.com>
2022-07-20 07:47:01 -0700

    print string in reverse

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Entry point
- * function that prints string followed by  new line
+ * _puts_recursion - prints a string
  * @s: string parameter
- * Return: on Success 0
  */
 void _puts_recursion(char *s)
 {
-if (*s == '\0')
-{
-_putchar('\n');
-return;
-}
-_putchar(*s);
-s++;
-_puts_recursion(s);
+	if (*s == '\0')
+	{
+		_putchar(10);
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
