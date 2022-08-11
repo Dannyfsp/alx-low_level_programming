97894ef52e025cd8535bbac876d7b196236937e1
Dannyfsp <femoscopilala@gmail.com>
2022-08-11 15:49:08 -0700

    function that prints before main function is executed

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 
-void first(void) __attribute__ ((constructor));
-
 /**
  * first - Entry point
  * prints a sentence before the main function is executed
  */
-void first(void)
+void __attribute__ ((constructor)) first()
 {
 	printf("You're beat! and yet, you must allow");
 	printf(",\nI bore my house upon my back my back!\n");
