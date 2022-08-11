dd587f0de51edc9c0c9645d20efb5c6b2ce63f7a
Dannyfsp <femoscopilala@gmail.com>
2022-08-11 15:10:16 -0700

    adds a new node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -25,12 +25,15 @@ list_t *add_node_end(list_t **head, const char *str)
 	new->next = NULL;
 
 	if (*head == NULL)
+	{
 		*head = new;
 		return (new);
+	}
 
 	while (temp->next)
 		temp = temp->next;
 
 	temp->next = new;
+
 	return (new);
 }
