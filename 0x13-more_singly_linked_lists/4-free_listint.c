c5622aa04a8e2fdb1e413b585eb66dc8a8def277
Dannyfsp <femoscopilala@gmail.com>
2022-08-15 17:14:22 -0700

    frees a linked list

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -3,8 +3,16 @@
 /**
  * free_listint - Entry point
  * function that frees a linked list
- * @head: head of the linked list
+ * @head: pointer to the head of the linked list
  */
 void free_listint(listint_t *head)
 {
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head->next;
+		free(head);
+		head = temp;
+	}
 }
