6fa24b295abdc2f0f686423dbc95cdd9657af879
Dannyfsp <femoscopilala@gmail.com>
2022-08-15 17:09:47 -0700

    adding a new node at the end of a linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -9,24 +9,23 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node;
-	listint_t *cursor = *head;
+	listint_t *temp, *newnode;
 
-	new_node = malloc(sizeof(listint_t));
-	if (new_node != NULL)
-	{
-		new_node->n = n;
-		new_node->next = *head;
-	}
-	else
+	newnode = malloc(sizeof(listint_t));
+	if (newnode == NULL)
 		return (NULL);
-	if (cursor != NULL)
-	{
-		while (cursor->next != NULL)
-			cursor = cursor->next;
-		cursor->next = new_node;
-	}
+	newnode->n = n;
+	newnode->next = NULL;
+	if (*head == NULL)
+		*head = newnode;
 	else
-		*head = new_node;
-	return (new_node);
+	{
+		temp = *head;
+		while (temp->next != NULL)
+		{
+			temp = temp->next;
+		}
+		temp->next = newnode;
+	}
+	return (newnode);
 }
---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+
+/**
+ * free_listint - Entry point
+ * function that frees a linked list
+ * @head: head of the linked list
+ */
+void free_listint(listint_t *head)
+{
+}
