1fc448d61d39c507ce7c4f18ac7503289cb80a99
Dannyfsp <femoscopilala@gmail.com>
2023-02-01 19:12:41 -0800

    advanced - interpolation / exponential

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,7 +10,4 @@ int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
-void print_array(int *array, size_t l, size_t r);
-int binary_search_p(int *array, size_t l, size_t r, int value);
-size_t min_value(size_t l, size_t r);
 #endif
